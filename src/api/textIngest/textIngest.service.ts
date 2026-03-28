bf5225f194d9b9ac5af496a63e4063510cf46130
Sanket <sanketjoshi96@gmail.com>
2026-03-28 14:35:24 +0530

    Adding fix for builds

--- src/api/textIngest/textIngest.service.ts
+++ src/api/textIngest/textIngest.service.ts
@@ -1,4 +1,4 @@
-import { randomUUID } from "node:crypto";
+import { randomUUID } from "crypto";
 
 import type { TextIngestPayload, TextIngestStructuredResult } from "../../types/textIngest.types";
 
